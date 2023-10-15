256a34b20e95838547205612d58ed2ddbf7d0e57
unknown <daviddimas80@gmail.com>
2023-10-15 19:30:26 +0700

    fix bug console

--- frontend/src/components/detailProduct.js
+++ frontend/src/components/detailProduct.js
@@ -38,6 +38,7 @@ const DetailProduct = () => {
         }
       )
       .then((res) => {
+        console.log(res.data);
         setDataProduct(res.data[0]);
         setDataProductCategories(res.data[1]);
         setIsLoading(false);
